bd3eb2ca243a17450ba2a641ab57517f946ed1d4
Jadir Xavier <49170436+JadirXavier@users.noreply.github.com>
2024-05-27 22:10:33 +0000

    Finished content view and read email highlight

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+
   document.querySelector('#compose-form').addEventListener('submit', send_email);
 
   // By default, load the inbox
@@ -15,7 +16,8 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
-
+  document.querySelector('#content-view').style.display = 'none';
+  
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
@@ -26,11 +28,14 @@ function load_mailbox(mailbox) {
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
-    console.log(emails)
+    console.log(emails);
+
+    document.querySelector('#emails-view').innerHTML = '';
+
     emails.forEach(email => {
       const email_element = document.createElement('div');
       email_element.innerHTML = `
-        <div class="card container">
+        <div class="card container" id="email-${email.id}">
           <div class="row">
             <div class="card-body col-sm-2">
               ${email.sender}
@@ -43,14 +48,25 @@ function load_mailbox(mailbox) {
             </div>
           </div>
         </div>
-        `
+      `;
+
       document.querySelector('#emails-view').append(email_element);
+
+      // Adds eventListener for each email
+      email_element.addEventListener('click', () => view_email(email.id));
+
+      if (email.read) {
+        document.getElementById(`email-${email.id}`).style.backgroundColor = '#94b5c4';
+      }
+
     });
   });
 
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#content-view').style.display = 'none';
+  
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -74,4 +90,44 @@ function send_email(event) {
         console.log(result);
       }
   });
+}
+
+function view_email(id) {
+  document.querySelector("#content-view").innerHTML=''
+  document.querySelector('#content-view').style.display = 'block';
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  
+  fetch(`/emails/${id}`)
+  .then(response => response.json())
+  .then(email => {
+    const content_element = document.createElement('div');
+    content_element.innerHTML = `
+      <div class="card border-0">
+      <div class="card-header px-0 py-3">
+        <p><strong>From:</strong> ${email.sender}</p>
+        <p><strong>To:</strong> ${email.recipients}</p>
+        <p><strong>Subject:</strong> ${email.subject}</p>
+        <p><strong>Timestamp:</strong> ${email.timestamp}</p>
+        <a href="#" class="btn btn-sm btn-outline-primary">Reply</a>
+      </div>
+      <div class="card-body">
+        <p class="card-text">${email.body}</p>
+      </div>
+    </div>
+    `;
+
+    if(!email.read) {
+      fetch(`/emails/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            read: true
+        })
+      })
+    }
+
+    document.querySelector('#content-view').append(content_element);
+  });
+
+ 
 }
